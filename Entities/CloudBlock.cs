9b45f080f16b55b6aa02c49fcd6e9e7c5a2a5cda
tkavner <ominousfire@gmail.com>
2022-07-08 23:30:25 -0400

    Initial commit

---
+++ Entities/CloudBlock.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xna.Framework;
+using Monocle;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Celeste.Mod.LylyraHelper.Entities
+{
+    class CloudBlock : JumpThru
+    {
+        public CloudBlock(Vector2 position, int width, int height, bool safe)
+        : base(position, width, safe)
+        {
+            base.Collider = new Hitbox(width, height);
+        }
+
+        public CloudBlock(EntityData data, Vector2 vector2) : this(data.Position + vector2, data.Int("width"), data.Int("height"), true)
+        {
+
+        }
+
+        public override void Update()
+        {
+            base.Update();
+
+        }
+    }
+}
