d2069e7b9477a6094ce073b1fa519806cef11bc5
tkavner <ominousfire@gmail.com>
2023-09-13 20:41:59 -0700

    ANOTHER UPDATE

--- Effects/ASHWind.cs
+++ Effects/ASHWind.cs
@@ -16,28 +16,43 @@ namespace LylyraHelper.Effects
     {
         private class Wind
         {
-            internal static int Points = 300;
             private static Vector2 endPoint;
             private float thicc;
             public Vector2[] curve;
-            public float percent;
+            public float percent = 75F;
             public Vector2 startingPoint;
+            public int pointsPerWind;
 
-            public static Wind MakeWind(Vector2 startingPoint, Random rand, float initAngle, float speed, float twist, float bend)
+            public static Wind MakeWind(Vector2 startingPoint, Random rand, float initAngle, float speed, float twist, float bend, int pointsPerWind, float maxBend)
             {
                 initAngle *= (float)Math.PI / 180;
                 Wind wind = new Wind();
-                Vector2[] curve = wind.curve = new Vector2[Points];
+                wind.pointsPerWind = pointsPerWind;
+                Vector2[] curve = wind.curve = new Vector2[pointsPerWind];
                 Vector2 nextPoint = wind.startingPoint = startingPoint;
                 float nextAngle = initAngle;
+                float nextBend = bend;
                 List<Vector2> points = new List<Vector2>();
-                for (int i = 0; i < Points; i++)
+                for (int i = 0; i < pointsPerWind / 2; i++)
                 {
                     points.Add(nextPoint);
                     nextPoint += new Vector2((float)Math.Cos(nextAngle), (float)Math.Sin(nextAngle)) * speed;
-                    nextAngle += bend;
-                    bend += rand.NextFloat() * twist - twist / 2;
+                    nextAngle += nextBend;
+                    nextBend = Calc.Clamp(nextBend, -maxBend, maxBend);
+                    nextBend += rand.NextFloat() * twist - twist / 2;
 
+                }
+
+                nextPoint = startingPoint;
+                nextAngle = initAngle;
+                nextBend = bend;
+                for (int i = 0; i < pointsPerWind / 2; i++)
+                {
+                    nextPoint -= new Vector2((float)Math.Cos(nextAngle), (float)Math.Sin(nextAngle)) * speed;
+                    nextAngle -= nextBend;
+                    nextBend -= rand.NextFloat() * twist - twist / 2;
+                    nextBend = Calc.Clamp(nextBend, -maxBend, maxBend);
+                    points.Insert(0, nextPoint); 
                 }
                 wind.curve = points.ToArray();
 
@@ -52,16 +67,16 @@ namespace LylyraHelper.Effects
             {
                 int indexWeight = 100;
                 //percents are out of a 200 point system right?! this actually occurs at 150% progress on our 200 point system
-                if (percent - indexWeight + indexWeight * (Points - v) / Points > 100)
+                if (percent - indexWeight + indexWeight * (pointsPerWind - v) / pointsPerWind > 100)
                 {
-                    var num1 = Calc.Clamp(200 - percent + (indexWeight * v / Points), 0, 100) / 100;
+                    var num1 = Calc.Clamp(200 - percent + (indexWeight * v / pointsPerWind), 0, 100) / 100;
                     num1 = num1 < 0.3 ? 0 : num1;
                     num1 = num1 > 0.5 ? 0.5F : num1;
                     return num1 * thicc;
                 } 
                 else
                 {
-                    var num1 = Calc.Clamp(percent - indexWeight + indexWeight * (Points - v) / Points, 0, 100) / 100;
+                    var num1 = Calc.Clamp(percent - indexWeight + indexWeight * (pointsPerWind - v) / pointsPerWind, 0, 100) / 100;
                     num1 = num1 < 0.3 ? 0 : num1;
                     num1 = num1 > 0.5 ? 0.5F : num1;
                     return num1 * thicc;
@@ -75,10 +90,9 @@ namespace LylyraHelper.Effects
         }
 
         //twist = jerk / acceleration change, bend = starting acceleration
-        public ASHWind(Vector2 startingPoint, int numWinds, float initAngle, float angleVariance, float speed, float speedVariance, float twist, float bend, float frequency, float lifespan)
+        public ASHWind(float initAngle, float angleVariance, float speed, float speedVariance, float twist, float bend, float frequency, float lifespan, float maxBend, int pointsPointWind)
         {
-            rand = new Random();
-            this.numWinds = numWinds;
+            rand = Calc.Random;
             this.initAngle = initAngle;
             this.angleVarience = angleVariance;
             this.speed = speed;
@@ -87,6 +101,8 @@ namespace LylyraHelper.Effects
             this.frequency = frequency;
             this.speedVarience = speedVariance;
             this.windLifespan = lifespan;
+            this.maxBend = maxBend;
+            this.pointsPointWind = pointsPointWind;
             int vertecies = GetVertecies();
             vertices = new VertexPositionColor[vertecies];
         }
@@ -97,14 +113,14 @@ namespace LylyraHelper.Effects
 
         private int GetVertecies()
         {
-            return (int) (Math.Ceiling(frequency) * Math.Ceiling(windLifespan) * 3 * (Wind.Points * 2 - 2));
+            return (int) (Math.Ceiling(frequency) * Math.Ceiling(windLifespan) * 3 * (pointsPointWind * 2 - 2));
         }
 
+        internal int pointsPointWind = 300;
         private List<Wind> winds = new List<Wind>();
         private int vertexCount;
         private float windCounter;
         private Random rand;
-        private int numWinds;
         private float twist;
         private float bend;
         private float initAngle;
@@ -112,8 +128,9 @@ namespace LylyraHelper.Effects
         private float frequency;
 
         private float windLifespan = 7.5F;
+        private float maxBend;
         private float MAXPERCENT = 250; //percents are out of 500 now
-        private float angleVarience = 0.3F;
+        private float angleVarience = 0.0F;
         private float speedVarience;
         private Vector2 screenDimensions = new Vector2(384, 244);
 
@@ -127,13 +144,13 @@ namespace LylyraHelper.Effects
             Vector2 vector2 = new Vector2(0f - vector.Y, vector.X);
             int vertexCounter = 0;
             List<Wind> oldWinds = new List<Wind>();
+            Vector2 vector3 = -level.Camera.Position;
             foreach (Wind wind in winds)
             {
                 wind.percent += Engine.DeltaTime * 75 * 6.5F / windLifespan;
                 Vector2 startingPoint = wind.startingPoint;
                 float xRenderPosBase = level.Camera.X; //this gun need to change
                 float yRenderPosBase = level.Camera.Y;
-                Vector2 vector3 = new Vector2(0, 0);
 
 
                 Vector2 prevPoint = wind.curve[0];
@@ -185,8 +202,17 @@ namespace LylyraHelper.Effects
                 float angle = initAngle + rand.NextFloat() * angleVarience - angleVarience / 2F;
                 var num1 = PointOnEllipseFromAngle(screenDimensions, angle);
                 var num2 = TangentToEllipseAtPoint(screenDimensions, angle) * (rand.NextFloat() * 180 - 90);
-                Vector2 startPoint = screenDimensions / 2+ num1 + num2;
-                winds.Add(Wind.MakeWind(startPoint, rand, angle + 180F, speed + rand.NextFloat() * speedVarience - speedVarience / 2, twist, bend));
+                var normal = NormalToEllipseAtPoint(screenDimensions, angle);
+                var num4 = -normal * (rand.NextFloat() * screenDimensions.X / 2);
+                var num3 = level.Camera.Position;
+                Vector2 startPoint = screenDimensions / 2 + num1 + num2 + num3 + num4;
+                Vector2 playerSpeed = scene.Tracker.GetEntity<Player>().Speed;
+
+                float cosineInBase = (playerSpeed.X * normal.X + playerSpeed.Y * normal.Y) / playerSpeed.Length();//normal.Length() = 1, guarenteed.
+
+                float playerSpeedAdjustment = (float)scene.Tracker.GetEntity<Player>().Speed.Length() * cosineInBase;
+                if (playerSpeedAdjustment < 0) { playerSpeedAdjustment = 0; }
+                winds.Add(Wind.MakeWind(startPoint, rand, angle + 180F, speed + rand.NextFloat() * speedVarience - speedVarience / 2 + playerSpeedAdjustment, twist, bend, pointsPointWind, maxBend));
             }
             vertexCount = vertexCounter;
 
@@ -219,6 +245,19 @@ namespace LylyraHelper.Effects
             {
                 angle *= (float) Math.PI / 180;
                 return (new Vector2((float)Math.Tan(angle) * ellipseDims.X, -1 * ellipseDims.Y)).SafeNormalize();
+            }
+        }
+
+        public static Vector2 NormalToEllipseAtPoint(Vector2 ellipseDims, float angle)
+        {
+            if (Math.Abs(Mod(angle, 180) - 90) < float.Epsilon)
+            {
+                return new Vector2(0, 1);
+            }
+            else
+            {
+                angle *= (float)Math.PI / 180;
+                return (new Vector2((float)1 * ellipseDims.X, (float)Math.Tan(angle) * ellipseDims.Y)).SafeNormalize();
             }
         }
 
--- Loenn/effects/ashWind.lua
+++ Loenn/effects/ashWind.lua
@@ -7,24 +7,24 @@ ashWind.canForeground = true
 blendingModes = {"HSV", "RGB"}
 
 ashWind.fieldInformation = {
-    numWinds = {
-        fieldType = "integer"
-    },
     initAngle = {
         fieldType = "number"
-    }
+    },
+    pointsPointWind = {
+        fieldType = "integer"
+    },
 }
 
 ashWind.defaultData = {
-    x = 0.0,
-    y = 0.0,
-    numWinds = 5,
-    initAngle = 3.4,
+    initAngle = 0,
     initAngleVariance = 0.1,
     speed = 10.0,
-    twist = 1.0,
-    bend = 1.0,
-    frequency = 2,
-    speedVariance = 0.1
+    twist = 0.001,
+    bend = 0.0,
+    frequency = 2.0,
+    speedVariance = 0.1,
+    pointsPointWind = 600,
+    windLifespan = 15,
+    maxBend = 0.02
 }
 return ashWind
