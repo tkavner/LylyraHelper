8ed5ba4240be3317a2baa7fa5aeb49e8de2c56ee
tkavner <ominousfire@gmail.com>
2023-09-16 18:27:05 -0700

    Update BigCloud.cs

--- Entities/BigCloud.cs
+++ Entities/BigCloud.cs
@@ -18,13 +18,14 @@ namespace Celeste.Mod.LylyraHelper.Entities
         private DynData<Cloud> cloudData;
         private Sprite sprite;
 
+        private float timer;
         public BigCloud(EntityData data, Vector2 offset) : base(data.Position + offset, data.Bool("fragile", true))
         {
             this.Collider.Width = 64;
             base.Collider.Position.X = -32f;
             base.Collider.Position.Y = -data.Int("offsetY", 5);
             this.fragile = data.Bool("fragile", true); //defaults to true because all BigClouds used to be fragile before adding option
-            
+            this.timer = Calc.Random.NextFloat() * 4f;
         }
 
         public override void Added(Scene scene)
@@ -39,6 +40,20 @@ namespace Celeste.Mod.LylyraHelper.Entities
             sprite.CenterOrigin();
             cloudData["sprite"] = sprite;
             Add(sprite);
+        }
+
+        public override void Update()
+        {
+            base.Update();
+            timer += Engine.DeltaTime;
+            if (GetPlayerRider() != null)
+            {
+                sprite.Position = Vector2.Zero;
+            }
+            else
+            {
+                sprite.Position = Calc.Approach(sprite.Position, new Vector2(0f, (float)Math.Sin(timer * 2f)) * 1.2F, Engine.DeltaTime * 4f); //the bigger sprite means we need a slightly different Approach()
+            }
         }
     }
 }
