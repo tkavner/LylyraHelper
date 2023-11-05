60b318315edf4cbe15895d77fa28e7b6a9525892
tkavner <ominousfire@gmail.com>
2023-11-04 17:39:40 -0700

    Fix placement names

--- Loenn/entities/KnifeSpikes.lua
+++ Loenn/entities/KnifeSpikes.lua
@@ -15,13 +15,12 @@ local spikeOptions = {
         sliceOnImpact = false,
         sliceableEntityTypes = ""
     },
-    placementName = "LylyraHelper/KnifeSpikes",
     directionNames = {
         up = "LylyraHelper/KnifeSpikesUp", 
         down = "LylyraHelper/KnifeSpikesDown", 
         left = "LylyraHelper/KnifeSpikesLeft", 
         right = "LylyraHelper/KnifeSpikesRight"
-    }
+    },
 }
 -- for each spike direction, we'll let lönn's spike helper generate most of what we need and won't need to take care of
 -- then we can replace or modify what was already generated to finish up the plugins
