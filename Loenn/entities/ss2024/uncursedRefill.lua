d9511e7d5119d1f9608237d6613964d44e5bdac1
tim <ominousfire@gmail.com>
2026-06-04 21:39:49 -0700

    Make Cursed and Uncursed Refills reskinnable

--- Loenn/entities/ss2024/uncursedRefill.lua
+++ Loenn/entities/ss2024/uncursedRefill.lua
@@ -1,5 +1,6 @@
 local helpers = require("mods").requireFromPlugin("helpers")
 
+local consts = require("mods").requireFromPlugin("consts")
 local refill = {}
 
 refill.name = "LylyraHelper/SS2024/UncursedRefill"
@@ -8,10 +9,12 @@ refill.placements = {
     {
         name = "Uncursed Refill",
         data = helpers.createPlacementData('1', {
-            oneUse = false
+            oneUse = false,
+            textureLocation="objects/LylyraHelper/ss2024/uncursedRefill"
         })
     }
 }
+refill.ignoredFields = consts.ignoredFields
 
 function refill.texture(room, entity)
     return "objects/LylyraHelper/ss2024/uncursedRefill/idle00"
